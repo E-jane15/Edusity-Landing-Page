8002a4330bf9e19b931bedf159787f6578ca0c62
E-jane15 <janeebaimay@gmail.com>
2024-08-15 08:45:35 +0100

    worked on testimonials and contact sections

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
 
 const App = () => {
   return (
@@ -18,6 +20,9 @@ const App = () => {
          <Title subTitle='Gallery' title='Campus Photos' />
          <Campus/>
          <Title subTitle='TESTIMONIALS' title='What Student Says' />
+         <Testimonials/>
+         <Title subTitle='Contact Us' title='Get in Touch' />
+         <Contact/>
       </div>
     </div>
   )
